7ab596f91ea44fe8db573cbdbea692b7145f5290
Anton Honcharov <4eyalush@gmail.com>
2016-07-21 00:46:52 +0300

    initial commit

---
+++ src/script.js
@@ -0,0 +1,8 @@
+let buttons = document.querySelectorAll('button')
+
+let object1 = { a: 1, b: true, c: 'q'}
+
+for (let index = 0; index < buttons.length; index++) {
+  buttons[index].innerHTML = index
+  buttons[index].onclick = (e) => {console.log(index)}
+}
