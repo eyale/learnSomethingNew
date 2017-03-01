adf6983796a2a77aace746c6dd70f95dd59900b3
antong <antong@netvertise.com>
2017-03-01 09:56:11 +0200

    adding the play video on scroll script

--- src/react_redux_binding.js
+++ src/react_redux_binding.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider,connect} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducers';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 // __REDUX_DEVTOOLS_EXTENSION__ это нужно для редакс девтулс
+// composeWithDevTools - добавляет девтулс ко всему что мы запишем внутрь
+
 const store = createStore(
     reducer
-    , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    , composeWithDevTools(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
